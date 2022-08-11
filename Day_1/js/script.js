716b08f77c73f80db6d670ebd3a7e3afd7d9fc12
Tristan <tfierros.edhs@gmail.com>
2022-08-10 19:15:17 -0700

    added more js

---
+++ Day_1/js/script.js
@@ -0,0 +1,23 @@
+no = false;
+yes = true;
+
+// boolean 
+
+abc = 'abc'
+a = 'a';
+
+// string
+
+one = 1;
+// number
+
+// organization
+object = {
+    name: 'john',
+    age: 30,
+    isPresent: true;
+}
+// object
+
+array = [1, 2, 3, 4, 5];
+// array
